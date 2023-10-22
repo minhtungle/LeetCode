b0e7d1eacf6f77a0f50bf4baec8a2adf86753d0b
minhtungle <tunglm@huongvietgroup.com>
2023-10-22 10:44:15 +0700

    ok

---
+++ 383. Ransom Note/RansomNote/Program.cs
@@ -0,0 +1,28 @@
+﻿bool CanConstruct(string ransomNote, string magazine) {
+    char[] ransomNote_ = ransomNote.ToCharArray();
+    char[] magazine_ = magazine.ToCharArray();
+    int ransomNote_Length = ransomNote_.Length;
+    int magazine_Length = magazine_.Length;
+    bool canConstruct = false;
+    int ransomNote_CheckPosition = 0;
+    if (magazine_Length < ransomNote_Length) return false;
+    for (int i = 0; i < magazine_Length; i++) {
+        if (ransomNote_Length < (ransomNote_CheckPosition + 1)) break;
+        char _ransomNote = ransomNote_[ransomNote_CheckPosition];
+        char _magazine = magazine_[i];
+        if (_magazine == _ransomNote) {
+            canConstruct = true;
+            ransomNote_CheckPosition++;
+        } else {
+            if (canConstruct) i--;
+            canConstruct = false;
+            ransomNote_CheckPosition = 0;
+        };
+    };
+    return canConstruct;
+};
+bool a = CanConstruct(
+    ransomNote: "fffbfg",
+    magazine: "effjfggbffjdgbjjhhdegh"
+    );
+Console.WriteLine(a);
